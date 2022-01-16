b39a324c393d9bdbe938288bac6e1e7beff6bc09
kianyang <ken.lee.kianyang@gmail.com>
2022-01-16 10:02:48 +0800

    Solution for exercise 1

--- script.py
+++ script.py
@@ -1,26 +1,34 @@
-from collections import defaultdict
+import itertools
 import time
 
 if __name__ == "__main__":
     tian_horses = input("Input for Tian: ")
     king_horses = input("Input for King: ")
     start_time = time.time()
-    list_of_tian_horses = map(int,tian_horses.split())
-    list_of_king_horses = map(int,king_horses.split())
-    zipped_list = zip(list_of_tian_horses, list_of_king_horses)
-    list_of_zipped_pairs = list(zipped_list)
-    scores = []
-    for index, zipped_elem in enumerate(list_of_zipped_pairs):
-        print(zipped_elem)
-        if zipped_elem[0] > zipped_elem[1]:
-            scores.append("tian")
-        elif zipped_elem[0] < zipped_elem[1]:
-            scores.append("king")
-        else:
-            scores.append("draw")
-    tian_score = scores.count("tian") - scores.count("king")
-    king_score = scores.count("king") - scores.count("tian")
-    end_time = time.time()
-    print("max_tian: ", tian_score)
-    print("max_king: ", king_score)
+    list_of_tian_horses = list(map(int,tian_horses.split()))
+    list_of_king_horses = list(map(int,king_horses.split()))
+
+    # Reference: https://www.kite.com/python/answers/how-to-get-all-unique-combinations-of-two-lists-in-python
+    permutation_list = itertools.permutations(list_of_tian_horses, len(list_of_king_horses))
+    all_combinations = [list(zip(permutation, list_of_king_horses)) for permutation in permutation_list]
+
+    max_king = -999 # To cater for situation where either side lost all matches
+    max_tian = -999
+    for index, combination in enumerate(all_combinations):
+        tian_score = 0
+        king_score = 0
+        for pair in combination:
+            if pair[0] > pair[1]:
+                tian_score += 1
+                king_score -= 1
+            elif pair[0] < pair[1]:
+                king_score += 1
+                tian_score -= 1
+        if tian_score > max_tian:
+            max_tian = tian_score
+        if king_score > max_king:
+            max_king = king_score
+    end_time = time.time()    
+    print("max_tian: ", max_tian)
+    print("max_king: ", max_king)
     print("Time spent: ", end_time - start_time)
